91712c1de7e29dcb450d60bd4cbf6a35eef82ce3
johnpooch <johnmcdowell0801@gmail.com>
2022-01-03 18:13:21 +0000

    Update allowed hosts

--- project/project/settings/prod.py
+++ project/project/settings/prod.py
@@ -5,7 +5,7 @@ import os
 
 DATABASES = {'default': dj_database_url.parse(os.environ['DATABASE_URL'])}
 
-ALLOWED_HOSTS = ['django_template.herokuapp.com']
+ALLOWED_HOSTS = ['johnpooch-django-template.herokuapp.com']
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
